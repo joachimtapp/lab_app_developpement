23e86719b00f46c3cbf9777aaf1ada27b230a4e5
joachim <joachim.tapparel@epfl.ch>
2018-11-27 15:08:41 +0100

    added nav drawer to gallery

--- GoproApp_wear/mobile/src/main/java/com/goproapp/goproapp_wear/MainActivity.java
+++ GoproApp_wear/mobile/src/main/java/com/goproapp/goproapp_wear/MainActivity.java
@@ -81,8 +81,8 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_home) {
+
         } else if (id == R.id.nav_gallery) {
             Intent intent= new Intent(MainActivity.this, GalleryActivity.class);
             MainActivity.this.startActivity(intent);
