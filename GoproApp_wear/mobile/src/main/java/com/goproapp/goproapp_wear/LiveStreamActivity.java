1462440a5f8a4e4f0889973609e3b739440c444c
Lennard <lennard.ludwig@gmail.com>
2019-01-04 14:04:17 +0100

    Added mode selection

--- GoproApp_wear/mobile/src/main/java/com/goproapp/goproapp_wear/LiveStreamActivity.java
+++ GoproApp_wear/mobile/src/main/java/com/goproapp/goproapp_wear/LiveStreamActivity.java
@@ -3,21 +3,16 @@ package com.goproapp.goproapp_wear;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -27,64 +22,161 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
 
 public class LiveStreamActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ImageButton mMenuDeployer;
+    private static boolean upDown = true;
+    private ArrayList<View> menus = new ArrayList<>();
+
+    private static final int MENU_PHOTO = 0;
+    private static final int MENU_VIDEO = 1;
+    private static final int MENU_BURST = 2;
+    private static final int MENU_POSITION = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_stream);
 
-        //Add Transparent drawer to have access to floating menu
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem item) {
-                        // Handle navigation view item clicks here.
-                        int id = item.getItemId();
-                        if (id != R.id.nav_livestream) {//exclude himself
-                            DrawerHandler dh = new DrawerHandler();
+        // Setup drawer and drawer button on the left
+        setupDrawer();
 
-                            Intent intent;
-                            intent = dh.SwitchActivity(id, LiveStreamActivity.this);
-                            LiveStreamActivity.this.startActivity(intent);
-                        }
-                        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-                        drawer.closeDrawer(GravityCompat.START);
-                        return true;
-                    }
-                });
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_live);
-        //set drawer button
-        setSupportActionBar(toolbar);
-        ActionBar actionbar = getSupportActionBar();
-        getSupportActionBar().setTitle(null);
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        // Setup side menu on the right
+        setupSideMenu();
+
+        // Setup EV adjustment bar on the bottom
+        setupEV();
+
+        // Setup mode menu at the center top
+        setupModeMenu();
+    }
+
+    private void setupModeMenu(){
+        ImageButton buttonPhoto = findViewById(R.id.modePhoto);
+        ImageButton buttonVideo = findViewById(R.id.modeVideo);
+        ImageButton buttonBurst = findViewById(R.id.modeBurst);
+        ImageButton buttonPosition = findViewById(R.id.modePosition);
 
 
-        // Add items to spinner of menu
-        Spinner spinner_res = findViewById(R.id.spinner_res);
-        Spinner spinner_FOV = findViewById(R.id.spinner_FOV);
-        Spinner spinner_FPS = findViewById(R.id.spinner_FPS);
+        buttonPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeMenu(MENU_PHOTO);
+            }
+        });
 
-        ArrayAdapter<CharSequence> adapter_res = ArrayAdapter.createFromResource(this, R.array.val_res, R.layout.spinner_item);
-        ArrayAdapter<CharSequence> adapter_FOV = ArrayAdapter.createFromResource(this, R.array.val_FOV, R.layout.spinner_item);
-        ArrayAdapter<CharSequence> adapter_FPS = ArrayAdapter.createFromResource(this, R.array.val_FPS, R.layout.spinner_item);
+        buttonVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeMenu(MENU_VIDEO);
+            }
+        });
 
-        adapter_res.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        adapter_FOV.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        adapter_FPS.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        buttonBurst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeMenu(MENU_BURST);
+            }
+        });
 
-        spinner_res.setAdapter(adapter_res);
-        spinner_FOV.setAdapter(adapter_FOV);
-        spinner_FPS.setAdapter(adapter_FPS);
+        buttonPosition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeMenu(MENU_POSITION);
+            }
+        });
+
+    }
+
+    private void changeMenu(int view){
+        // Set all invisible
+        for (int i = 0; i < menus.size(); i++){
+            menus.get(i).setVisibility(View.INVISIBLE);
+        }
+
+        // Set visible the corresponding view
+        menus.get(view).setVisibility(View.VISIBLE);
+
+        Resources r = getResources();
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,46.5F*view, r.getDisplayMetrics());
+
+        View boxView = findViewById(R.id.selectedMode);
+        ObjectAnimator animation = ObjectAnimator.ofFloat(boxView, "translationX", px);
+        animation.setDuration(500);
+        animation.setInterpolator(new AccelerateDecelerateInterpolator());
+        animation.start();
+    }
+
+    private void setupEV(){
+        SeekBar EV_bar = findViewById(R.id.EV_bar);
+        TextView EV_text = findViewById(R.id.EV_val);
+
+        // Place SeekBar in the middle
+        EV_bar.setProgress(4);
+
+        // Set text of TextView
+        EV_text.setText("EV : " + (EV_bar.getProgress() - 4.)/2);
+
+        // Set callback for SeekBar
+        EV_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                EV_text.setText("EV : " + (progress - 4.)/2);
+                //TODO : Send new parameter to GoPro
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        // Animate bar when user clicks on the text
+        EV_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                View view = findViewById(R.id.EV_view);
+                ObjectAnimator animator;
+                if(upDown){
+                    animator = ObjectAnimator.ofFloat(view, "translationY", 60);
+                } else {
+                    animator = ObjectAnimator.ofFloat(view, "translationY", 0);
+                }
+                upDown = !upDown;
+                animator.setDuration(700);
+                animator.setInterpolator(new AccelerateDecelerateInterpolator());
+                animator.start();
+            }
+        });
+    }
+
+    private void setupSideMenu(){
+
+        View menuPhoto = findViewById(R.id.menu_photo);
+        View menuVideo = findViewById(R.id.menu_video);
+        View menuBurst = findViewById(R.id.menu_burst);
+        View menuPosition = findViewById(R.id.menu_position);
+
+        menus.add(MENU_PHOTO, menuPhoto);
+        menus.add(MENU_VIDEO, menuVideo);
+        menus.add(MENU_BURST, menuBurst);
+        menus.add(MENU_POSITION, menuPosition);
+
+        menuPhoto.setVisibility(View.VISIBLE);
+        menuVideo.setVisibility(View.INVISIBLE);
+        menuBurst.setVisibility(View.INVISIBLE);
+        menuPosition.setVisibility(View.INVISIBLE);
+
+        setupSpinner();
 
 
         //Rotate button on the side of menu each time it is pressed and slide in and out the menu
@@ -108,8 +200,6 @@ public class LiveStreamActivity extends AppCompatActivity {
                     px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dip,r.getDisplayMetrics());
                 }
 
-                //mMenuDeployer.animate().rotation(deg).setInterpolator(new AccelerateDecelerateInterpolator());
-
                 animation_button = ObjectAnimator.ofFloat(mMenuDeployer, "rotation", deg);
                 animation_menu = ObjectAnimator.ofFloat(view, "translationX", px);
 
@@ -123,7 +213,25 @@ public class LiveStreamActivity extends AppCompatActivity {
                 animation_button.start();
             }
         });
+    }
 
+    private void setupSpinner(){
+        // Add items to spinner of menu
+        Spinner spinner_res = findViewById(R.id.spinner_res);
+        Spinner spinner_FOV = findViewById(R.id.spinner_FOV);
+        Spinner spinner_FPS = findViewById(R.id.spinner_FPS);
+
+        ArrayAdapter<CharSequence> adapter_res = ArrayAdapter.createFromResource(this, R.array.val_res, R.layout.spinner_item);
+        ArrayAdapter<CharSequence> adapter_FOV = ArrayAdapter.createFromResource(this, R.array.val_FOV, R.layout.spinner_item);
+        ArrayAdapter<CharSequence> adapter_FPS = ArrayAdapter.createFromResource(this, R.array.val_FPS, R.layout.spinner_item);
+
+        adapter_res.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter_FOV.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter_FPS.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner_res.setAdapter(adapter_res);
+        spinner_FOV.setAdapter(adapter_FOV);
+        spinner_FPS.setAdapter(adapter_FPS);
 
         spinner_res.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -163,29 +271,37 @@ public class LiveStreamActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    private void setupDrawer() {
+        //Add Transparent drawer to have access to floating menu
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        // Handle navigation view item clicks here.
+                        int id = item.getItemId();
+                        if (id != R.id.nav_livestream) {//exclude himself
+                            DrawerHandler dh = new DrawerHandler();
 
-        SeekBar EV_bar = findViewById(R.id.EV_bar);
-        TextView EV_text = findViewById(R.id.EV_val);
-
-        EV_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                EV_text.setText("EV : " + (progress - 4.)/2);
-                //TODO : Send new parameter to GoPro
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
+                            Intent intent;
+                            intent = dh.SwitchActivity(id, LiveStreamActivity.this);
+                            LiveStreamActivity.this.startActivity(intent);
+                        }
+                        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                        drawer.closeDrawer(GravityCompat.START);
+                        return true;
+                    }
+                });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_live);
+        //set drawer button
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        getSupportActionBar().setTitle(null);
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
     }
 
     @Override
