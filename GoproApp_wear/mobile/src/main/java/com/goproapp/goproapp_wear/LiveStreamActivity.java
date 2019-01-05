daf6e7d2f31643832d87c0324296ace500b19ab6
Lennard <lennard.ludwig@gmail.com>
2019-01-05 15:51:43 +0100

    Added video an photo menu (completed)

--- GoproApp_wear/mobile/src/main/java/com/goproapp/goproapp_wear/LiveStreamActivity.java
+++ GoproApp_wear/mobile/src/main/java/com/goproapp/goproapp_wear/LiveStreamActivity.java
@@ -16,9 +16,11 @@ import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -29,9 +31,28 @@ public class LiveStreamActivity extends AppCompatActivity {
     private ImageButton mMenuDeployer;
     private static boolean down_EV = true;
     private ArrayList<View> menus = new ArrayList<>();
+
+    private Spinner spinner_res_video;
+    private Spinner spinner_FPS_video;
+    private Spinner spinner_FOV_video;
+
+    private Spinner spinner_res_photo;
+    private Spinner spinner_FOV_photo;
+
     private GoProCombinations goProCombinations;
-    private ArrayList<String> FPS_spinner;
-    private ArrayList<String> FOV_spinner;
+    private ArrayList<String> FPS_spinner_video;
+    private ArrayList<String> FOV_spinner_video;
+    private Switch switchWB_video;
+    private Switch switchISO_video;
+    private SeekBar seekBarWB_video;
+    private SeekBar seekBarISO_video;
+
+    private Switch switchWB_photo;
+    private Switch switchISO_photo;
+    private Switch switchShutter_photo;
+    private SeekBar seekBarWB_photo;
+    private SeekBar seekBarISO_photo;
+    private SeekBar seekBarShutter_photo;
 
     private static final int MENU_PHOTO = 0;
     private static final int MENU_VIDEO = 1;
@@ -180,8 +201,6 @@ public class LiveStreamActivity extends AppCompatActivity {
         menuBurst.setVisibility(View.INVISIBLE);
         menuPosition.setVisibility(View.INVISIBLE);
 
-        setupSpinner();
-
 
         //Rotate button on the side of menu each time it is pressed and slide in and out the menu
         mMenuDeployer = findViewById(R.id.menuDeployer);
@@ -217,86 +236,12 @@ public class LiveStreamActivity extends AppCompatActivity {
                 animation_button.start();
             }
         });
+
+        videoMenuSetup();
+
+        photoSetup();
     }
 
-    private void setupSpinner(){
-
-        // Add items to spinner of menu
-        Spinner spinner_res = findViewById(R.id.spinner_res);
-        Spinner spinner_FOV = findViewById(R.id.spinner_FOV);
-        Spinner spinner_FPS = findViewById(R.id.spinner_FPS);
-
-        ArrayAdapter<CharSequence> adapter_res = ArrayAdapter.createFromResource(this, R.array.val_res, R.layout.spinner_item);
-
-        FOV_spinner = goProCombinations.getFov("1080p");
-        FPS_spinner = goProCombinations.getFPS("1080p");
-
-        ArrayAdapter<String> adapter_FOV = new ArrayAdapter<>(this, R.layout.spinner_item, FOV_spinner);
-        ArrayAdapter<String> adapter_FPS = new ArrayAdapter<>(this,  R.layout.spinner_item, FPS_spinner);
-
-        adapter_res.setDropDownViewResource(R.layout.spinner_dropdown);
-        adapter_FOV.setDropDownViewResource(R.layout.spinner_dropdown);
-        adapter_FPS.setDropDownViewResource(R.layout.spinner_dropdown);
-
-        spinner_res.setAdapter(adapter_res);
-        spinner_FOV.setAdapter(adapter_FOV);
-        spinner_FPS.setAdapter(adapter_FPS);
-
-        // Set initial state
-        spinner_res.setSelection(4);
-        spinner_FPS.setSelection(5);
-        spinner_FOV.setSelection(2);
-
-        spinner_res.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                //Toast.makeText(LiveStreamActivity.this, "New resolution selected : " + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
-                //TODO : send new parameter to GoPro
-                FOV_spinner = goProCombinations.getFov(spinner_res.getSelectedItem().toString());
-                FPS_spinner = goProCombinations.getFPS(spinner_res.getSelectedItem().toString());
-
-                ArrayAdapter<String> adapter_FOV = new ArrayAdapter<>(parent.getContext(), R.layout.spinner_item, FOV_spinner);
-                ArrayAdapter<String> adapter_FPS = new ArrayAdapter<>(parent.getContext(),  R.layout.spinner_item, FPS_spinner);
-
-                adapter_FOV.setDropDownViewResource(R.layout.spinner_dropdown);
-                adapter_FPS.setDropDownViewResource(R.layout.spinner_dropdown);
-
-                spinner_FOV.setAdapter(adapter_FOV);
-                spinner_FPS.setAdapter(adapter_FPS);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        spinner_FOV.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                //Toast.makeText(LiveStreamActivity.this, "New FOV selected : " + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
-                //TODO : send new parameters to GoPro
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        spinner_FPS.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                //Toast.makeText(LiveStreamActivity.this, "New FPS rate selected : " + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
-                //TODO : Send data to GoPro.
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-    }
 
     private void setupDrawer() {
         //Add Transparent drawer to have access to floating menu
@@ -360,5 +305,210 @@ public class LiveStreamActivity extends AppCompatActivity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void photoSetup(){
+        spinner_res_photo = findViewById(R.id.spinner_res_photo);
+        spinner_FOV_photo = findViewById(R.id.spinner_FOV_photo);
+
+        ArrayAdapter<CharSequence> adapter_res = ArrayAdapter.createFromResource(getApplicationContext(), R.array.val_res_photo, R.layout.spinner_item);
+        ArrayAdapter<CharSequence> adapter_FOV = ArrayAdapter.createFromResource(getApplicationContext(), R.array.val_FOV, R.layout.spinner_item);
+
+        adapter_res.setDropDownViewResource(R.layout.spinner_dropdown);
+        adapter_FOV.setDropDownViewResource(R.layout.spinner_dropdown);
+
+        spinner_res_photo.setAdapter(adapter_res);
+        spinner_FOV_photo.setAdapter(adapter_FOV);
+
+        switchWB_photo = findViewById(R.id.switchWB_photo);
+        switchISO_photo = findViewById(R.id.switchISO_photo);
+        switchShutter_photo = findViewById(R.id.switchShutter_photo);
+
+        seekBarWB_photo = findViewById(R.id.seekBarWB_photo);
+        seekBarISO_photo = findViewById(R.id.seekBarISO_photo);
+        seekBarShutter_photo = findViewById(R.id.seekBarShutter_photo);
+
+        seekBarWB_photo.setEnabled(false);
+        seekBarISO_photo.setEnabled(false);
+        seekBarShutter_photo.setEnabled(false);
+
+        setupCallbackPhoto();
+    }
+
+    private void setupCallbackPhoto(){
+
+        spinner_res_photo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //TODO : send data to GopPro
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner_FOV_photo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //TODO send data to GoPro
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        switchWB_photo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    seekBarWB_photo.setEnabled(false);
+                } else {
+                    seekBarWB_photo.setEnabled(true);
+                }
+            }
+        });
+
+        switchISO_photo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    seekBarISO_photo.setEnabled(false);
+                } else {
+                    seekBarISO_photo.setEnabled(true);
+                }
+            }
+        });
+
+        switchShutter_photo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    seekBarShutter_photo.setEnabled(false);
+                } else {
+                    seekBarShutter_photo.setEnabled(true);
+                }
+            }
+        });
+    }
+
+    private void videoMenuSetup(){
+
+        // Add items to spinner of menu
+        spinner_res_video = findViewById(R.id.spinner_res_video);
+        spinner_FOV_video = findViewById(R.id.spinner_FOV_video);
+        spinner_FPS_video = findViewById(R.id.spinner_FPS_video);
+
+        ArrayAdapter<CharSequence> adapter_res = ArrayAdapter.createFromResource(getApplicationContext(), R.array.val_res_video, R.layout.spinner_item);
+
+        FOV_spinner_video = goProCombinations.getFov("1080p");
+        FPS_spinner_video = goProCombinations.getFPS("1080p");
+
+        ArrayAdapter<String> adapter_FOV = new ArrayAdapter<>(getApplicationContext(), R.layout.spinner_item, FOV_spinner_video);
+        ArrayAdapter<String> adapter_FPS = new ArrayAdapter<>(getApplicationContext(), R.layout.spinner_item, FPS_spinner_video);
+
+        adapter_res.setDropDownViewResource(R.layout.spinner_dropdown);
+        adapter_FOV.setDropDownViewResource(R.layout.spinner_dropdown);
+        adapter_FPS.setDropDownViewResource(R.layout.spinner_dropdown);
+
+        spinner_res_video.setAdapter(adapter_res);
+        spinner_FOV_video.setAdapter(adapter_FOV);
+        spinner_FPS_video.setAdapter(adapter_FPS);
+
+        // Set initial state
+        spinner_res_video.setSelection(4);
+        spinner_FPS_video.setSelection(5);
+        spinner_FOV_video.setSelection(2);
+
+        // Setup WB
+        switchWB_video = findViewById(R.id.switchWB_video);
+        seekBarWB_video = findViewById(R.id.seekBarWB_video);
+        seekBarWB_video.setEnabled(false);
+
+        switchISO_video = findViewById(R.id.switchISO_video);
+        seekBarISO_video = findViewById(R.id.seekBarISO_video);
+        seekBarISO_video.setEnabled(false);
+
+        setupCallbackVideo();
+
+    }
+
+
+    private void setupCallbackVideo(){
+        spinner_res_video.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(LiveStreamActivity.this, "New resolution selected : " + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                //TODO : send new parameter to GoPro
+                FOV_spinner_video = goProCombinations.getFov(spinner_res_video.getSelectedItem().toString());
+                FPS_spinner_video = goProCombinations.getFPS(spinner_res_video.getSelectedItem().toString());
+
+                ArrayAdapter<String> adapter_FOV = new ArrayAdapter<>(parent.getContext(), R.layout.spinner_item, FOV_spinner_video);
+                ArrayAdapter<String> adapter_FPS = new ArrayAdapter<>(parent.getContext(),  R.layout.spinner_item, FPS_spinner_video);
+
+                adapter_FOV.setDropDownViewResource(R.layout.spinner_dropdown);
+                adapter_FPS.setDropDownViewResource(R.layout.spinner_dropdown);
+
+                spinner_FOV_video.setAdapter(adapter_FOV);
+                spinner_FPS_video.setAdapter(adapter_FPS);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner_FOV_video.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(LiveStreamActivity.this, "New FOV selected : " + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                //TODO : send new parameters to GoPro
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner_FPS_video.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(LiveStreamActivity.this, "New FPS rate selected : " + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                //TODO : Send data to GoPro.
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        switchWB_video.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    seekBarWB_video.setEnabled(false);
+                } else {
+                    seekBarWB_video.setEnabled(true);
+                }
+            }
+        });
+
+        switchISO_video.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    seekBarISO_video.setEnabled(false);
+                } else {
+                    seekBarISO_video.setEnabled(true);
+                }
+            }
+        });
+
     }
 }
