9237acf3cf1aa90618c98e4c1bbd5867c6fa1509
joachim <joachim.tapparel@epfl.ch>
2019-01-11 17:44:42 +0100

    image download gopro

--- GoproApp_wear/mobile/src/main/java/com/goproapp/goproapp_wear/ImgData.java
+++ GoproApp_wear/mobile/src/main/java/com/goproapp/goproapp_wear/ImgData.java
@@ -5,9 +5,11 @@ import android.graphics.Bitmap;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 
 public class ImgData {
+    protected String name;
     protected String date;
     protected String bpm;
     protected LatLng latLng;
     protected String imgUrl;
-    protected Bitmap img;
+    protected String imgString;
+    protected Bitmap imgBmp;
 }
